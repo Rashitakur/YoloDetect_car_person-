4a60071597727e1479038b2ecd2c513578c4a0d5
unknown <thakurrashita@gmail.com>
2025-06-26 16:40:58 +0530

    Added pages/index and config files

---
+++ DEPLOY/next.config.js
@@ -0,0 +1,8 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  allowedDevOrigins: ['http://localhost:3000', 'http://192.168.8.37:3000'], // optional
+};
+
+module.exports = nextConfig;
